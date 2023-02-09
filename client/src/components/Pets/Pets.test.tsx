8fb556f1482fa14e35f770faa8c12deb7eefa737
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-02-09 17:46:19 -0500

    Change PetsContextProvider to Pets Component and refactor Pets tests with helper functions

--- client/src/components/Pets/Pets.test.tsx
+++ client/src/components/Pets/Pets.test.tsx
@@ -1,62 +1,8 @@
-import { render, screen, within } from '@testing-library/react';
-import { Pets } from './Pets';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
-import { pets as petsMock } from '../../mocks/pets';
-import { CardProps } from '../Card/Card';
-import userEvent from '@testing-library/user-event';
-import { PetsContextProviderMock } from '../setupTest';
+import { screen } from '@testing-library/react';
+import { selectFilters, clickFavoriteCardButton } from '../setupTest';
+import { setupMSWServer } from '../setupTest';
 
-type Response = CardProps[];
-
-const server = setupServer(
-  rest.get<Response>('http://localhost:4000/pets', (req, res, ctx) => {
-    return res(ctx.json(petsMock));
-  })
-);
-
-type SelectFiltersObj = {
-  favorite?: string;
-  gender?: string;
-  animalType?: string;
-};
-
-const selectFiltersReturningCards = async ({ favorite, gender, animalType }: SelectFiltersObj) => {
-  const cards: HTMLElement[] = await screen.findAllByRole('article');
-
-  const selectFavoriteElement: HTMLSelectElement = screen.getByLabelText(/favorite/i);
-  const selectGenderElement: HTMLSelectElement = screen.getByLabelText(/gender/i);
-  const selectAnimalTypeElement: HTMLSelectElement = screen.getByLabelText(/woof or miau?/i);
-
-  if (favorite !== undefined) {
-    userEvent.selectOptions(selectFavoriteElement, favorite);
-  }
-  if (gender !== undefined) {
-    userEvent.selectOptions(selectGenderElement, gender);
-  }
-  if (animalType !== undefined) {
-    userEvent.selectOptions(selectAnimalTypeElement, animalType);
-  }
-
-  return cards;
-};
-
-beforeEach(async () => {
-  render(
-    <PetsContextProviderMock>
-      <Pets />
-    </PetsContextProviderMock>
-  );
-});
-beforeAll(() => {
-  server.listen();
-});
-afterEach(() => {
-  server.resetHandlers();
-});
-afterAll(() => {
-  server.close();
-});
+setupMSWServer();
 
 describe('Pets', () => {
   test('should show right amount of card components', async () => {
@@ -66,54 +12,41 @@ describe('Pets', () => {
   // Integration tests:
   // favorite filter
   test('should filter for male pets when click male on gender select', async () => {
-    // const selectGenderElement: HTMLSelectElement = screen.getByLabelText(/gender/i);
-    // userEvent.selectOptions(selectGenderElement, 'male');
-    const cards: HTMLElement[] = await selectFiltersReturningCards({ gender: 'male' });
+    const cards: HTMLElement[] = await selectFilters({ gender: 'male' });
     const maleCards: HTMLElement[] = screen.getAllByRole('article');
     expect(maleCards).toStrictEqual([cards[1], cards[3]]);
   });
 
   test('should filter for female pets when click female on gender select', async () => {
-    const cards: HTMLElement[] = await screen.findAllByRole('article');
-    const selectGenderElement: HTMLSelectElement = screen.getByLabelText(/gender/i);
-    userEvent.selectOptions(selectGenderElement, 'female');
+    const cards: HTMLElement[] = await selectFilters({ gender: 'female' });
+
     const femaleCards: HTMLElement[] = screen.getAllByRole('article');
     expect(femaleCards).toStrictEqual([cards[0], cards[2], cards[4]]);
   });
   // gender filter
   test('should filter for favorite pets when click favorite on favorite select', async () => {
-    const cards: HTMLElement[] = await screen.findAllByRole('article');
-    const selectFavoriteElement: HTMLSelectElement = screen.getByLabelText(/favorite/i);
-    userEvent.selectOptions(selectFavoriteElement, 'favorite');
+    const cards: HTMLElement[] = await selectFilters({ favorite: 'favorite' });
+
     const favoriteCards: HTMLElement[] = screen.getAllByRole('article');
     expect(favoriteCards).toStrictEqual([cards[0], cards[1]]);
   });
 
   test('should filter for not favorite pets when click not favorite on favorite select', async () => {
-    const cards: HTMLElement[] = await screen.findAllByRole('article');
-
-    const selectFavoriteElement: HTMLSelectElement = screen.getByLabelText(/favorite/i);
-    userEvent.selectOptions(selectFavoriteElement, 'not favorite');
+    const cards: HTMLElement[] = await selectFilters({ favorite: 'not favorite' });
 
     const notFavoriteCards: HTMLElement[] = screen.getAllByRole('article');
     expect(notFavoriteCards).toStrictEqual([cards[2], cards[3], cards[4]]);
   });
   // animal type filter
   test('should filter for dog type when click dog on animal-type select', async () => {
-    const cards: HTMLElement[] = await screen.findAllByRole('article');
-
-    const selectAnimalTypeElement: HTMLSelectElement = screen.getByLabelText(/woof or miau?/i);
-    userEvent.selectOptions(selectAnimalTypeElement, 'dog');
+    const cards: HTMLElement[] = await selectFilters({ animalType: 'dog' });
 
     const dogCards: HTMLElement[] = screen.getAllByRole('article');
     expect(dogCards).toStrictEqual([cards[0], cards[1], cards[2]]);
   });
 
   test('should filter for dog type when click dog on animal-type select', async () => {
-    const cards: HTMLElement[] = await screen.findAllByRole('article');
-
-    const selectAnimalTypeElement: HTMLSelectElement = screen.getByLabelText(/woof or miau?/i);
-    userEvent.selectOptions(selectAnimalTypeElement, 'cat');
+    const cards: HTMLElement[] = await selectFilters({ animalType: 'cat' });
 
     const catCards: HTMLElement[] = screen.getAllByRole('article');
     expect(catCards).toStrictEqual([cards[3], cards[4]]);
@@ -121,126 +54,99 @@ describe('Pets', () => {
 
   //favorite + gender filters + click on cards
   test('should filter for favorite and female pets when click favorite on favorite select and click female on gender select', async () => {
-    const cards: HTMLElement[] = await screen.findAllByRole('article');
-
-    const selectFavoriteElement: HTMLSelectElement = screen.getByLabelText(/gender/i);
-    userEvent.selectOptions(selectFavoriteElement, 'female');
-    const selectGenderElement: HTMLSelectElement = screen.getByLabelText(/favorite/i);
-    userEvent.selectOptions(selectGenderElement, 'favorite');
+    const cards: HTMLElement[] = await selectFilters({ favorite: 'favorite', gender: 'female' });
 
     const favoriteFemaleCards: HTMLElement[] = screen.getAllByRole('article');
     expect(favoriteFemaleCards).toStrictEqual([cards[0]]);
   });
 
   test('should filter for favorite and male pets when click favorite on favorite select and click male on gender select', async () => {
-    const cards: HTMLElement[] = await screen.findAllByRole('article');
-
-    const selectFavoriteElement: HTMLSelectElement = screen.getByLabelText(/gender/i);
-    userEvent.selectOptions(selectFavoriteElement, 'male');
-    const selectGenderElement: HTMLSelectElement = screen.getByLabelText(/favorite/i);
-    userEvent.selectOptions(selectGenderElement, 'favorite');
+    const cards: HTMLElement[] = await selectFilters({ favorite: 'favorite', gender: 'male' });
 
     const favoriteMaleCards: HTMLElement[] = screen.getAllByRole('article');
     expect(favoriteMaleCards).toStrictEqual([cards[1]]);
   });
 
   test('should filter for not favorite and female pets when click not favorite on favorite select and click female on gender select', async () => {
-    const cards: HTMLElement[] = await screen.findAllByRole('article');
-
-    const selectFavoriteElement: HTMLSelectElement = screen.getByLabelText(/gender/i);
-    userEvent.selectOptions(selectFavoriteElement, 'female');
-    const selectGenderElement: HTMLSelectElement = screen.getByLabelText(/favorite/i);
-    userEvent.selectOptions(selectGenderElement, 'not favorite');
+    const cards: HTMLElement[] = await selectFilters({ favorite: 'not favorite', gender: 'female' });
 
     const notFavoriteFemaleCards: HTMLElement[] = screen.getAllByRole('article');
     expect(notFavoriteFemaleCards).toStrictEqual([cards[2], cards[4]]);
   });
 
   test('should filter for not favorite and male pets when click not favorite on favorite select and click male on gender select', async () => {
-    const cards: HTMLElement[] = await screen.findAllByRole('article');
-
-    const selectFavoriteElement: HTMLSelectElement = screen.getByLabelText(/gender/i);
-    userEvent.selectOptions(selectFavoriteElement, 'male');
-    const selectGenderElement: HTMLSelectElement = screen.getByLabelText(/favorite/i);
-    userEvent.selectOptions(selectGenderElement, 'not favorite');
+    const cards: HTMLElement[] = await selectFilters({ favorite: 'not favorite', gender: 'male' });
 
     const notFavoriteMaleCards: HTMLElement[] = screen.getAllByRole('article');
     expect(notFavoriteMaleCards).toStrictEqual([cards[3]]);
   });
 
   test('should filter favorite cards after clicking the first not favorite card heart icon', async () => {
-    const cards: HTMLElement[] = await screen.findAllByRole('article');
-    const firstNotFavoriteCard = cards[2];
-    const firstNotFavoriteCardButton: HTMLButtonElement = within(firstNotFavoriteCard).getByRole('button');
-    const selectGenderElement: HTMLSelectElement = screen.getByLabelText(/favorite/i);
+    const cardsInitial: HTMLElement[] = await screen.findAllByRole('article');
+    clickFavoriteCardButton(cardsInitial[2]);
 
-    userEvent.click(firstNotFavoriteCardButton);
+    const cards: HTMLElement[] = await selectFilters({ favorite: 'favorite' });
 
-    userEvent.selectOptions(selectGenderElement, 'favorite');
     const favoriteCards: HTMLElement[] = screen.getAllByRole('article');
     expect(favoriteCards).toStrictEqual([cards[0], cards[1], cards[2]]);
   });
 
   test('should filter not favorite cards after clicking the first favorite card heart icon', async () => {
-    const cards: HTMLElement[] = await screen.findAllByRole('article');
-    const firstFavoriteCard = cards[0];
-    const firstFavoriteCardButton: HTMLButtonElement = within(firstFavoriteCard).getByRole('button');
-    const selectGenderElement: HTMLSelectElement = screen.getByLabelText(/favorite/i);
+    const cardsInitial: HTMLElement[] = await screen.findAllByRole('article');
 
-    userEvent.click(firstFavoriteCardButton);
+    clickFavoriteCardButton(cardsInitial[0]);
 
-    userEvent.selectOptions(selectGenderElement, 'not favorite');
+    const cards: HTMLElement[] = await selectFilters({ favorite: 'not favorite' });
+
     const notFavoriteCards: HTMLElement[] = screen.getAllByRole('article');
     expect(notFavoriteCards).toStrictEqual([cards[0], cards[2], cards[3], cards[4]]);
   });
 
   test('should not show any cards after selecting favorite and female and clicking heart button on the card', async () => {
-    const cards: HTMLElement[] = await screen.findAllByRole('article');
-    const selectFavoriteElement: HTMLSelectElement = screen.getByLabelText(/favorite/i);
-    const selectGenderElement: HTMLSelectElement = screen.getByLabelText(/gender/i);
-
-    userEvent.selectOptions(selectFavoriteElement, 'favorite');
-    userEvent.selectOptions(selectGenderElement, 'female');
-
-    const favoriteFemaleCard = cards[0];
-    const favoriteFemaleCardButton: HTMLButtonElement = within(favoriteFemaleCard).getByRole('button');
-    userEvent.click(favoriteFemaleCardButton);
+    const cards = await selectFilters({ favorite: 'favorite', gender: 'female' });
+    clickFavoriteCardButton(cards[0]);
 
     const favoriteFemaleCards = screen.queryAllByRole('article');
     expect(favoriteFemaleCards.length).toBe(0);
   });
 
   test('should show not favorite male cards after selecting not favorite and male and clicking heart button on the card ', async () => {
-    const cards: HTMLElement[] = await screen.findAllByRole('article');
-    const selectFavoriteElement: HTMLSelectElement = screen.getByLabelText(/favorite/i);
-    const selectGenderElement: HTMLSelectElement = screen.getByLabelText(/gender/i);
-
-    userEvent.selectOptions(selectFavoriteElement, 'not favorite');
-    userEvent.selectOptions(selectGenderElement, 'male');
-
-    const notFavoriteMaleCard = cards[3];
-    const notFavoriteMaleCardButton: HTMLButtonElement = within(notFavoriteMaleCard).getByRole('button');
-    userEvent.click(notFavoriteMaleCardButton);
+    const cards = await selectFilters({ favorite: 'not favorite', gender: 'male' });
+    clickFavoriteCardButton(cards[3]);
 
     const notFavoritMaleCards = screen.queryAllByRole('article');
     expect(notFavoritMaleCards.length).toBe(0);
   });
-
-  test('when first clicking heart button on Felix(first not favorite, female, cat) should show favorite female cat card after selecting favorite, female and cat selects', async () => {
+  //favorite + gender + animal-type filters + click on card
+  test('when first clicking heart button on Felix(first not favorite, female, cat) should show favorite female cat card after selecting favorite, female and cat options', async () => {
     const cards: HTMLElement[] = await screen.findAllByRole('article');
-    const selectFavoriteElement: HTMLSelectElement = screen.getByLabelText(/favorite/i);
-    const selectGenderElement: HTMLSelectElement = screen.getByLabelText(/gender/i);
-    const selectAnimalTypeElement: HTMLSelectElement = screen.getByLabelText(/woof or miau?/i);
 
-    const notFavoriteFemaleCatCard = cards[4];
-    const notFavoriteFemaleCatCardButton: HTMLButtonElement = within(notFavoriteFemaleCatCard).getByRole('button');
-    userEvent.click(notFavoriteFemaleCatCardButton);
-
-    userEvent.selectOptions(selectFavoriteElement, 'favorite');
-    userEvent.selectOptions(selectGenderElement, 'female');
-    userEvent.selectOptions(selectAnimalTypeElement, 'cat');
+    clickFavoriteCardButton(cards[4]);
+    await selectFilters({ favorite: 'favorite', gender: 'female', animalType: 'cat' });
 
     const favoriteFemaleCatCard = screen.getByRole('article');
     expect(favoriteFemaleCatCard).toBeInTheDocument();
+  });
+
+  test('when first clicking heart button on third card(not favorite, female, dog) should show (favorite female dog) cards after selecting favorite, female and cat options', async () => {
+    const cards: HTMLElement[] = await screen.findAllByRole('article');
+
+    clickFavoriteCardButton(cards[2]);
+    await selectFilters({ favorite: 'favorite', gender: 'female', animalType: 'dog' });
+
+    const favoriteFemaleDogCard = screen.getAllByRole('article');
+    expect(favoriteFemaleDogCard).toStrictEqual([cards[0], cards[2]]);
+  });
+  //favorite + gender + animal-type filters + click on multiple cards
+  test('when first clicking heart button on dogs cards then clicking heart button on last card should show then filter (not favorite male dog) should show one card', async () => {
+    const cards: HTMLElement[] = await screen.findAllByRole('article');
+
+    clickFavoriteCardButton(cards[0]);
+    clickFavoriteCardButton(cards[1]);
+    clickFavoriteCardButton(cards[2]);
+    await selectFilters({ favorite: 'not favorite', gender: 'male', animalType: 'dog' });
+
+    const favoriteFemaleDogCard = screen.getByRole('article');
+    expect(favoriteFemaleDogCard).toStrictEqual(cards[1]);
   });
 });
--- client/src/components/setupTest.tsx
+++ client/src/components/setupTest.tsx
@@ -1,17 +1,23 @@
 import { pets } from '../mocks/pets';
 import { PetsContextObj } from '../context/PetsContext';
 import { PetsContext } from '../context/PetsContext';
+import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Pets } from './Pets/Pets';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { pets as petsMock } from '../mocks/pets';
+import { CardProps } from './Card/Card';
 
-const mockFn1 = jest.fn();
-const mockFn2 = jest.fn();
-const mockFn3 = jest.fn();
+// Setup context provider mock:
+const mockFn = jest.fn();
 
 const contextValue: PetsContextObj = {
   pets: pets,
-  setPets: mockFn1,
+  setPets: mockFn,
   filteredPets: pets,
-  setFilteredPets: mockFn2,
-  updateFavorite: mockFn3,
+  setFilteredPets: mockFn,
+  updateFavorite: mockFn,
 };
 
 type PetsContextProviderMockProps = {
@@ -20,4 +26,65 @@ type PetsContextProviderMockProps = {
 
 export const PetsContextProviderMock = ({ children }: PetsContextProviderMockProps) => {
   return <PetsContext.Provider value={contextValue}>{children}</PetsContext.Provider>;
+};
+
+type SelectFiltersObj = {
+  favorite?: string;
+  gender?: string;
+  animalType?: string;
+};
+
+// Helper functions:
+export const selectFilters = async ({ favorite, gender, animalType }: SelectFiltersObj) => {
+  const cards: HTMLElement[] = await screen.findAllByRole('article');
+
+  const selectFavoriteElement: HTMLSelectElement = screen.getByLabelText(/favorite/i);
+  const selectGenderElement: HTMLSelectElement = screen.getByLabelText(/gender/i);
+  const selectAnimalTypeElement: HTMLSelectElement = screen.getByLabelText(/woof or miau?/i);
+
+  if (favorite !== undefined) {
+    userEvent.selectOptions(selectFavoriteElement, favorite);
+  }
+  if (gender !== undefined) {
+    userEvent.selectOptions(selectGenderElement, gender);
+  }
+  if (animalType !== undefined) {
+    userEvent.selectOptions(selectAnimalTypeElement, animalType);
+  }
+
+  return cards;
+};
+
+export const clickFavoriteCardButton = (cardElement: HTMLElement) => {
+  const selectedCard = cardElement;
+  const selectedCardButton: HTMLButtonElement = within(selectedCard).getByRole('button');
+  userEvent.click(selectedCardButton);
+};
+
+// Setup setupMSWServer for Pets test file:
+export const setupMSWServer = () => {
+  type Response = CardProps[];
+
+  const server = setupServer(
+    rest.get<Response>('http://localhost:4000/pets', (req, res, ctx) => {
+      return res(ctx.json(petsMock));
+    })
+  );
+
+  beforeEach(() => {
+    render(
+      <PetsContextProviderMock>
+        <Pets />
+      </PetsContextProviderMock>
+    );
+  });
+  beforeAll(() => {
+    server.listen();
+  });
+  afterEach(() => {
+    server.resetHandlers();
+  });
+  afterAll(() => {
+    server.close();
+  });
 };
